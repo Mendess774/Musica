c29d7b0084f98e7719f142c0fc7eef4253b036e3
Felipe Mendes <you@example.com>
2024-05-26 17:17:57 -0300

    exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,155 @@
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.Write("\n");
+Console.Write("Imprevistos");
+Console.Write("\n");
+
+Console.ResetColor();
+
+Console.Write("\n");
+
+Console.Write(@"Mais um fim de semana, os bares lotados
+E eu tentando ligar
+Os meus amigos ligando, e eu aqui te esperando
+E você onde está?
+
+Eu tenho aqui na minha frente o seu rosto
+Vejo a tua boca mas não posso te beijar
+Teu olhar fixamente me vigia
+De uma fotografia
+
+Eu coloquei a melhor roupa pra te ver
+Mudei meu corte de cabelo pra te impressionar
+Mas já saquei que nesta noite vai chover
+Bem no meu olhar
+
+Só preciso de um olá, ou alô, ou coisa assim
+Porque assim eu vou saber o que eu tô fazendo aqui
+Imprevistos acontecem, meu coração não te esquece
+Faça o seu lembrar de mim
+
+Só preciso de um olá, ou alô, ou coisa assim
+Porque assim eu vou saber o que eu tô fazendo aqui
+Imprevistos acontecem, meu coração não te esquece
+Faça o seu lembrar de mim");
+
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+Console.Write("\n");
+Console.Write("Don't");
+Console.Write("\n");
+
+Console.ResetColor();
+
+Console.Write("\n");
+
+Console.Write(@"Don't, don't play with her, don't be dishonest
+Still not understandin' this logic
+I'm back and I'm better
+I want you bad as ever
+Don't let me just let up
+I wanna give you better
+Baby, it's whatever
+Somebody gotta step up
+Girl, I'm that somebody, so I'm next up
+Be damned if I let him catch up
+It's easy to see that you're fed up
+I am on a whole 'nother level
+Girl, he only fucked you over 'cause you let him
+
+Fuck him, girl, I guess he didn't know any better
+Girl, that man didn't show any effort
+Do all I can just to show you you're special
+Certain, it's your love that holds me together
+Lately you say he been killin' the vibe
+Gotta be sick of this guy
+Pull up, skurt, get in the ride
+Left hand is steerin', the other is grippin' your thigh
+Light up a spliff and get high
+Shawty, you deserve what you've been missin'
+Lookin' at you I'm thinkin' he must be trippin'
+Play this song for him, tell him: Just listen
+Don't
+
+Girl, you said he keeps on playin' games
+And his lovin' ain't the same
+I don't know what to say, but
+What a shame
+If you were mine, you would not get the same
+If you were mine, you would top everything
+Suicide in the drop, switchin' lanes
+And that thing so fire, baby, no propane
+Got good pussy, girl, can I be frank?
+To keep it one-hundred, girl, I ain't no saint
+But he the only reason that I'm feelin' this way
+Givin' you the world, baby, when you get space
+Pen game get me laid, baby, that's penetrate, oh, baby");
+
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write("\n");
+Console.Write("Purple");
+Console.Write("\n");
+
+Console.ResetColor();
+
+Console.Write("\n");
+
+Console.Write(@"Apaga a luz
+Enquanto sente meu toque
+Enquanto o chão se dissolve
+Enquanto tudo me aborrece
+Demônios testam minha fé
+
+Ela quer me hipnotizar, meduza
+Vem me seduzir
+Tudo me acusa
+
+Se eu ouço purple ah, ah
+Todas as vezes te olho purple ah, ah
+Tudo a minha volta ficou purple ah, ah
+Sim, a noite eu vejo tudo purple ah, ah
+Minha volta gira em volta
+
+Tira os meus pés do chão (do chão)
+Vou pra outra dimensão
+Purple ah, ah, uh, ye
+Purple ah, ah
+
+Baby, eu não me encaixo
+Ela me bota pra baixo
+Tudo me soa desprezo
+Canto minha dor no compasso
+Ela me bota pra baixo
+Livre, mas me sinto preso
+
+Ela quer me priorizar
+Baby, me ajuda
+Sua autoestima
+Não vale a minha
+
+sobe bem devagar
+(Ey) bad, (ey) pode te dominar
+
+Se eu ouço purple ah, ah
+Toda noite eu te vejo purple ah, ah
+Sim, a noite eu vejo tudo purple
+
+Tira meus pés do chão
+Vou pra outra dimensão
+Toda noite eu te olho purple ah, ah
+Tudo gira em volta e fica purple ah, ah
+
+Ey, ey
+Sei que isso tudo é stress, isso só me entristece
+Ey, ey
+Lembro de você a noite, isso não me aquece
+
+Ey, ey
+Sei que isso tudo é stress, isso só me entristece
+Ey, ey
+Lembro de você a noite, isso não me aquece");
